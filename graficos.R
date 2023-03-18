211f3b5f7a68a9a770228433096633403f7cf19a
Andressa Soares <andressalopezss@gmail.com>
2023-03-18 18:53:43 -0300

    update

--- graficos.R
+++ graficos.R
@@ -2,66 +2,69 @@ library(ggplot2)
 library(tidyr)
 library(tidyverse)
 library(esquisse)
+library(ggsave)
+library(RColorBrewer)
 
-dados <- read_delim("ALUNOS_2017.csv",delim = ";", escape_double = FALSE, trim_ws = TRUE)
+dados <- read_delim("BD_ESTUDO.csv",delim = ";", escape_double = FALSE, trim_ws = TRUE)
 dados_transformados <- dados %>% 
-  select(CO_ALUNO, TP_SITUACAO, TP_TURNO, TP_SEXO, TP_COR_RACA, IDADE) %>% 
-  mutate(TP_TURNO=ifelse(TP_TURNO==1,"MORNING",
-                         ifelse(TP_TURNO==3,"NIGHT",
-                                ifelse(TP_TURNO==2,"AFTERNOON",
-                                       ifelse(TP_TURNO==4,"FULL-TIME",
-                                              ifelse(is.na(TP_TURNO)==TRUE,"NA","NA"))))),
-         TP_SITUACAO= ifelse(TP_SITUACAO==4,"EVADED",
-                             ifelse(TP_SITUACAO==5,"EVADED",      
-                                    ifelse(TP_SITUACAO==6,"GRADUATED","CENSORSHIP"))),
-         TP_COR_RACA=ifelse(TP_COR_RACA==0,"NAO DECLARADO",
-                            ifelse(TP_COR_RACA==1,"BRANCA",
-                                   ifelse(TP_COR_RACA==2,"PRETA",
-                                          ifelse(TP_COR_RACA==3,"PARDA",
-                                                 ifelse(TP_COR_RACA==4,"AMARELA","INDIGENA"))))),
-         TP_SEXO=(ifelse(TP_SEXO==1,"FEMALE","MALE")))
-
-ggplot(data = dados_transformados, aes(Status, Students)) +
-  geom_col(aes(fill = Status))
+  select(CO_ALUNO, CO_ALUNO_SITUACAO, NO_EVENTO, CO_TURNO_ALUNO, IN_SEXO_ALUNO, CO_COR_RACA_ALUNO, NU_IDADE_ALUNO) %>% 
+  mutate(CO_TURNO_ALUNO=ifelse(CO_TURNO_ALUNO==1,"MORNING",
+                         ifelse(CO_TURNO_ALUNO==3,"NIGHT",
+                                ifelse(CO_TURNO_ALUNO==2,"AFTERNOON",
+                                       ifelse(CO_TURNO_ALUNO==4,"FULL-TIME",
+                                              ifelse(is.na(CO_TURNO_ALUNO)==TRUE,NA,NA))))),
+         CO_ALUNO_SITUACAO= ifelse(CO_ALUNO_SITUACAO==4,"EVADED",
+                             ifelse(CO_ALUNO_SITUACAO==5,"EVADED",      
+                                    ifelse(CO_ALUNO_SITUACAO==6,"GRADUATED","CENSORSHIP"))),
+         CO_COR_RACA_ALUNO=ifelse(CO_COR_RACA_ALUNO==0,"NAO DECLARADO",
+                            ifelse(CO_COR_RACA_ALUNO==1,"BRANCA",
+                                   ifelse(CO_COR_RACA_ALUNO==2,"PRETA",
+                                          ifelse(CO_COR_RACA_ALUNO==3,"PARDA",
+                                                 ifelse(CO_COR_RACA_ALUNO==4,"AMARELA","INDIGENA"))))),
+         SEXO_ALUNO=(ifelse(IN_SEXO_ALUNO==0,"MALE","FEMALE"))) %>% 
+  drop_na(CO_TURNO_ALUNO)
 
 attach(dados_transformados)
 
-sumarizados <- dados_transformados %>% 
-  group_by(TP_SEXO) %>% 
-  summarise(Students = n_distinct(CO_ALUNO))
+g_turno <- ggplot(data = dados_transformados, aes(CO_TURNO_ALUNO, na.rm = TRUE)) +
+  geom_bar(aes(fill = CO_ALUNO_SITUACAO), position = "fill") +
+  theme_minimal()
 
-as_tibble(table(TP_SITUACAO))
-
-g_turno <- ggplot(data = dados_transformados, aes(TP_TURNO)) +
-  geom_bar(aes(fill = TP_SITUACAO), position = "fill") +
-  theme_linedraw() 
-
-
-library(ggplot2)
-
-shift <- ggplot(dados_transformados) +
- aes(x = TP_TURNO, fill = TP_SITUACAO) +
+shift <- ggplot(dados_transformados, na.rm = TRUE) +
+ aes(x = CO_TURNO_ALUNO, fill = CO_ALUNO_SITUACAO) +
  geom_bar(position = "fill") +
-  scale_fill_brewer(palette = "YlOrRd")+
- labs(x = "SHIFT", y = "STUDENS PROPORTION", fill = NULL) +
+  scale_fill_brewer(palette = "YlOrRd",direction = -1)+
+ labs(x = "SHIFT", y = "PROPORTION OF STUDENTS", fill = NULL) +
  theme_minimal()
+shift
+ggsave("shift_plot.pdf", shift,width = 6, height=3)
+
 
 gender <- ggplot(dados_transformados) +
-  aes(x = TP_SEXO, fill = TP_SITUACAO) +
+  aes(x = SEXO_ALUNO, fill = CO_ALUNO_SITUACAO) +
   geom_bar(position = "fill") +
-  scale_fill_brewer(palette = "YlOrRd")+
-  labs(x = "GENDER", y = "STUDENS PROPORTION", fill = NULL) +
+  scale_fill_brewer(palette = "YlOrRd",direction = -1)+
+  labs(x = "GENDER", y = "PROPORTION OF STUDENTS", fill = NULL) +
   theme_minimal()
+gender
+ggsave("gender_plot.pdf", gender,width = 6, height=3)
 
 age <- ggplot(dados_transformados) +
-  aes(x = IDADE, fill = TP_SITUACAO) +
+  aes(x = NU_IDADE_ALUNO, fill = CO_ALUNO_SITUACAO) +
   geom_histogram(bins = 30L) +
-  scale_fill_brewer(palette = "YlOrRd")+
-  labs(x = "AGE", y = "STUDENS", fill = "SITUATION")
+  scale_fill_brewer(palette = "YlOrRd", direction = -1)+
+  labs(x = "AGE", y = "NUMBER OF STUDENS", fill = NULL)+
   theme_minimal()
+age
+ggsave("age_plot.pdf", age,width = 6, height=3)
+
 
 situation <- ggplot(dados_transformados) +
-    aes(x = TP_SITUACAO) +
-    geom_bar(fill = brewer.pal(3,"YlOrRd")) +
-    labs(x = NULL, y = "STUDENS")+
+    aes(x = CO_ALUNO_SITUACAO) +
+    geom_bar(fill = rev(brewer.pal(3,"YlOrRd"))) +
+    labs(x = NULL, y = "NUMBER OF STUDENTS")+
     theme_minimal()
+situation
+
+ggsave("situation_plot.pdf", situation,width = 6, height=3)
+
